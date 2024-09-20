5243b011be2bfe776f9cf926761288a34b92ad1a
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-09-19 21:11:36 -0300

    fix: removed videos.tsx and added bg-nivia.png

--- components/ui/videos.tsx
+++
@@ -1,7 +0,0 @@
-const videoLinks = [
-    "https://youtu.be/ybovbqhaILs",
-    "https://youtu.be/GfoAc4ZR0To",
-    "https://youtu.be/UUv9BprLNyc",
-    "https://youtu.be/cXML84x9AlM",
-];
-export default videoLinks;
