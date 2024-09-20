557226dd809abdf4cf968ce7de652ec5fc6724e4
Rafael Caslu Henry Cassiano Luiz <147253148+RCaslu@users.noreply.github.com>
2024-09-20 18:36:11 +0000

    fix: fixed flicker and updated page render

--- app/page.tsx
+++ app/page.tsx
@@ -6,35 +6,29 @@ import Section3 from "./_pages/section3";
 import Section4 from "./_pages/section4";
 import Section5 from "./_pages/section5";
 import Section1Mobile from "./_pages/section1Mobile";
-import useWindowSize from './_pages/mobileIdentifier'; 
 import Section2Mobile from "./_pages/section2Mobile";
 import Section3Mobile from "./_pages/section3Mobile";
 import Section4Mobile from "./_pages/section4Mobile";
 import Section5Mobile from "./_pages/section5Mobile";
+import './style.css';
 
 export default function Home() {
-  const size = useWindowSize();
-  const isMobile = size.width <= 768;
-  
   return (
     <>
-      {isMobile ? (
-        <>
+      <div className="desktop">
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
+      </div>
+      <div className="mobile">
         <Section1Mobile />
         <Section2Mobile />
         <Section3Mobile />
         <Section4Mobile />
         <Section5Mobile />
-        </>
-        ) : (
-          <>
-          <Section1 />
-          <Section2 />
-          <Section3 />
-          <Section4 />
-          <Section5 />
-          </>
-          )}
+      </div>
     </>
   );
 }
